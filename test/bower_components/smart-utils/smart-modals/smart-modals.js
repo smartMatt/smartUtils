09de4450c7084f9e1739142b1cbadf07f6eb01c1
Matt Ross <matt@smartsrc.com>
2014-09-23 12:03:45 -0700

    adjusted modal templates

--- test/bower_components/smart-utils/smart-modals/smart-modals.js
+++ test/bower_components/smart-utils/smart-modals/smart-modals.js
@@ -8,7 +8,7 @@ smartModals.factory('smartModals', ['$modal', '$sce', function($modal, $sce) {
       config = config || {};
 
       var template;
-      var url = config.templateUrl || '/smart-modals/alert-modal.html';
+      var url = config.templateUrl || './bower_components/smart-utils/smart-modals/templates/alert-modal.html';
 
       if(config.template) {
         url = null;
@@ -45,7 +45,7 @@ smartModals.factory('smartModals', ['$modal', '$sce', function($modal, $sce) {
     },
     confirm: function(options) {
 
-      var url = options.templateUrl || '/bower_components/smart-utils/smart-modals/templates/confirm-modal.html';
+      var url = options.templateUrl || './bower_components/smart-utils/smart-modals/templates/confirm-modal.html';
       options.message = $sce.trustAsHtml(options.message);
       options.closeClass = options.closeClass || "btn btn-success";
       options.close = options.close || "Yes";
--- test/routes/modal-demo/modal-demo-ctrl.js
+++ test/routes/modal-demo/modal-demo-ctrl.js
@@ -1,5 +1,32 @@
 smartDemo.controller('ModalDemoCtrl', function($scope, smartModals) {
 
 
-  smartModals.alert({message: "hey"});
+
+  $scope.alertModal = function () {
+
+    var options = {
+      title: "Alert Modal",
+      message: "Warning this is an alert.",
+      confirm: "Ok"
+    }
+
+    smartModals.alert(options);
+
+  }
+
+
+  $scope.confirmModal = function () {
+
+    var options = {
+      title: "Confirm Modal",
+      message: "Do you approve this modal?",
+      close: "Yes",
+      dismiss: "No"
+    }
+
+    smartModals.confirm(options)
+
+  }
+
+
 })
